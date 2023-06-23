dbbae2d5702e0a9b0a05e3504b232b1663f8b965
Mommy Me <mkm0182@yahoo.com>
2023-06-23 12:39:02 -0500

    adding copy of code from non-working repository

---
+++ script.js
@@ -0,0 +1,26 @@
+// Assignment code here
+var generateBtn = document.querySelector("#generate");
+  
+
+ var lowerCase = ["a", ];
+ var upperCase = [];
+ var numbers = [];
+ var specialCharacters = [];
+
+
+// Get references to the #generate element
+function generatePassword () {
+  
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
